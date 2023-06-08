1bc5b8aa799e72a83e5eaeb47fad85b5238cd6a7
ggantos <ggantos@ucar.edu>
2023-06-07 22:29:07 -0600

    fixing typo

--- setup_conda_tf.sh
+++ setup_conda_tf.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-conda env create -f tf_212.yml
+conda env create -f tf212_env.yml
 conda activate tf212gpu
 mkdir -p $CONDA_PREFIX/etc/conda/activate.d
 mkdir -p $CONDA_PREFIX/etc/conda/deactivate.d
