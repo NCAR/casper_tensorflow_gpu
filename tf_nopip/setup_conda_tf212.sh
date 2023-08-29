ea91baf379142383d386b6bf97aa3efb7621f00a
Daniel Howard <dhoward@ucar.edu>
2023-08-29 12:49:56 -0600

    Corrected merge conflict by re-committing all updates, including no pip and pip separated TF installs for 2.11-2.13 version.

---
+++ tf_nopip/setup_conda_tf212.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+module load conda
+
+CONDA_OVERRIDE_CUDA="11.8" mamba env create -f tf212_nopip_env.yml
+
+conda activate tf212gpu
+
+mkdir -p $CONDA_PREFIX/etc/conda/activate.d
+mkdir -p $CONDA_PREFIX/etc/conda/deactivate.d
+
+cp activate_env_vars.sh $CONDA_PREFIX/etc/conda/activate.d/env_vars.sh
+cp deactivate_env_vars.sh $CONDA_PREFIX/etc/conda/deactivate.d/env_vars.sh
+
+conda deactivate
